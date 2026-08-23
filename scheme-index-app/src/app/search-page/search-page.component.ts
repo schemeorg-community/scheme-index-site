91a62bdedae485dd8f28f9678729814b6a8d589d
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2026-08-23 21:25:39 +0300

    work

--- scheme-index-app/src/app/search-page/search-page.component.ts
+++ scheme-index-app/src/app/search-page/search-page.component.ts
@@ -1,4 +1,4 @@
-import { ReplaySubject, Observable, map, combineLatest, first } from 'rxjs';
+import { ReplaySubject, Observable, map, combineLatest, first, tap, shareReplay, switchMap } from 'rxjs';
 import { Component, ViewChild, ElementRef, inject, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { IndexService } from '../index.service';
@@ -36,7 +36,7 @@ export class SearchPageComponent {
 
     filterset = '';
     indexQuery: Observable<IndexQuery>;
-    response = new ReplaySubject<IndexResponse>(1);
+    response: Observable<IndexResponse>;
     filterpaneParams: Observable<FilterpaneParams>;
     pagerParams: Observable<PagerParams | null>;
     results: Observable<SearchItem[]>;
@@ -78,29 +78,38 @@ export class SearchPageComponent {
                 returns
             };
         }));
-        this.indexQuery.subscribe(q => {
-            svc.query(q).pipe(first()).subscribe(resp => this.response.next(resp));
-            // collapse filter panel for small screens on query
-            if (this.svc.facetCollapseOnSearch()) {
-                this.facetCollapsed = true;
-            }
-            if (this.resultsContainer)
-                this.resultsContainer.nativeElement.scroll({ 
-                    top: 0, 
-                    left: 0
-                });
-        });
+        this.response = this.indexQuery.pipe(
+            switchMap(q => svc.query(q)),
+            tap(() => {
+                // collapse filter panel for small screens on query
+                if (this.svc.facetCollapseOnSearch()) {
+                    this.facetCollapsed = true;
+                }
+                if (this.resultsContainer)
+                    this.resultsContainer.nativeElement.scroll({ 
+                        top: 0, 
+                        left: 0
+                    });
+            }),
+            shareReplay(1)
+        );
         this.filterpaneParams = combineLatest([this.indexQuery, this.response]).pipe(map(([query, response]) => {
             return {
                 query,
                 response
             };
         }));
-        this.pagerParams = combineLatest([this.indexQuery, this.response]).pipe(map(([query, response]) => {
+        this.pagerParams = combineLatest([
+            this.indexQuery, 
+            this.response,
+            svc.pageSize$
+        ]).pipe(map(([query, response, pageSize]) => {
+            if (pageSize == 0)
+                return null;
             if (!response.total)
                 return null;
             return {
-                pageSize: 40,
+                pageSize: pageSize,
                 total: response.total,
                 page: query.page || 1
             };
